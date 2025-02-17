941764c2fbcf6c7a6833bc3a66882787ab260100
Tommi Herranen <tommi.herranen@siili.com>
2025-02-17 19:39:17 +0200

    Separate portman functions

--- PortmanHttpTrigger/__init__.py
+++ PortmanHttpTrigger/__init__.py
@@ -1,7 +1,12 @@
+import sys
+import os
 import logging
-
 import azure.functions as func
 
+# Ensure parent-dir is in the Python module search path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+from portman import main as portman_main
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -18,7 +23,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
+        portman_main(req)
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Portman function triggered successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
