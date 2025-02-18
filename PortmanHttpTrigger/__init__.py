e5aec1d9e4e95f68a6935ddeddf17262a38ecc75
Tommi Herranen <tommi.herranen@siili.com>
2025-02-18 08:42:33 +0200

    Move portman functions

--- PortmanHttpTrigger/__init__.py
+++ PortmanHttpTrigger/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import azure.functions as func
 
-from portman import main as portman_main
+from PortmanHttpTrigger.portman import main as portman_main
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
