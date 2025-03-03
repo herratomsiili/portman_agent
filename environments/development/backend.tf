3185f885e58f355641d99a4b2211d0c711266317
Tommi Herranen <tommi.herranen@siili.com>
2025-03-03 14:15:24 +0200

    Fix backend.tf

--- environments/development/backend.tf
+++ environments/development/backend.tf
@@ -1,10 +1,11 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "therranen-portman-rgxx"
-    storage_account_name = "therranenportmanstorage"
-    container_name       = "tfstate"
-    #key                  = "development.tfstate"
-    key = "terraform.tfstate"
-    #use_oidc             = true
-  }
+  #backend "azurerm" {
+  #  resource_group_name  = "therranen-portman-rgxx"
+  #  storage_account_name = "therranenportmanstorage"
+  #  container_name       = "tfstate"
+  #  #key                  = "development.tfstate"
+  #  key = "terraform.tfstate"
+  #  #use_oidc             = true
+  #}
+  backend "azurerm" {}
 }
