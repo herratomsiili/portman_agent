6036e8e8accee9b8023eddb64c68cd0fc8532987
Tommi Herranen <tommi.herranen@siili.com>
2025-03-04 10:05:20 +0200

    Remove resource group from azure deploy stack for now

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -1,11 +1,11 @@
 # Define Resource Group
-resource "azurerm_resource_group" "default" {
-  name     = var.resource_group_name
-  location = var.location
-  tags = {
-    owner = "tommi.herranen@siili.com"
-  }
-}
+#resource "azurerm_resource_group" "default" {
+#  name     = var.resource_group_name
+#  location = var.location
+#  tags = {
+#    owner = "tommi.herranen@siili.com"
+#  }
+#}
 
 # Define PostgreSQL Flexible Server
 resource "azurerm_postgresql_flexible_server" "postgres" {
