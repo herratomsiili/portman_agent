9b54f06f6d6ff14295e1c942d43b1dbc41b4a5e7
Tommi Herranen <tommi.herranen@siili.com>
2025-03-05 15:41:07 +0200

    Fix service plan property name

--- modules/appservice/main.tf
+++ modules/appservice/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_function_app" "function_app" {
   location                   = var.location
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  service_plan_id            = azurerm_service_plan.function_plan.id
+  app_service_plan_id        = azurerm_service_plan.function_plan.id
   os_type                    = "linux"
   app_settings = {
     "APPINSIGHTS_INSTRUMENTATIONKEY" = var.ai_instrumentation_key
