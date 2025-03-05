7e7db401ddc130a62511c983ffc7691599f4220d
Tommi Herranen <tommi.herranen@siili.com>
2025-03-05 15:32:37 +0200

    Add instrumentation key as parameter

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -19,11 +19,12 @@ module "postgres" {
 }
 
 module "appservice" {
-  source               = "../../modules/appservice"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  naming_prefix        = var.naming_prefix
-  storage_account_name = var.storage_account_name
+  source                 = "../../modules/appservice"
+  resource_group_name    = var.resource_group_name
+  location               = var.location
+  naming_prefix          = var.naming_prefix
+  storage_account_name   = var.storage_account_name
+  ai_instrumentation_key = module.monitoring.ai_instrumentation_key
 }
 
 module "monitoring" {
